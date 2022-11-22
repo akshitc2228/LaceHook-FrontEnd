a28197deb814bea0af955b23d0b952a8634ddb62
Akshit Chaturvedi <akshitc2228@gmail.com>
2022-11-22 20:29:48 +0000

    sidebar added

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,41 @@
+import "./sidebar.css"
+import {RssFeed, Chat, PlayCircle, Group, Event, SportsEsports, Bookmark} from "@mui/icons-material"
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+              <RssFeed className="sidebarIcon"/>
+              <span className="sidebarListItemText">Feed</span>
+          </li>
+          <li className="sidebarListItem">
+              <Group className="sidebarIcon"/>
+              <span className="sidebarListItemText">Friends</span>
+          </li>
+          <li className="sidebarListItem">
+              <Chat className="sidebarIcon"/>
+              <span className="sidebarListItemText">Chats</span>
+          </li>
+          <li className="sidebarListItem">
+              <PlayCircle className="sidebarIcon"/>
+              <span className="sidebarListItemText">Videos</span>
+          </li>
+          <li className="sidebarListItem">
+              <Event className="sidebarIcon"/>
+              <span className="sidebarListItemText">Events</span>
+          </li>
+          <li className="sidebarListItem">
+              <SportsEsports className="sidebarIcon"/>
+              <span className="sidebarListItemText">Games</span>
+          </li>
+          <li className="sidebarListItem">
+              <Bookmark className="sidebarIcon"/>
+              <span className="sidebarListItemText">Bookmarks</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
