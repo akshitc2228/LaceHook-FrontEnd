bf16d37970f4b3b1445bbbc4415ff8e85eef9fe0
Akshit Chaturvedi <akshitc2228@gmail.com>
2022-11-25 23:04:49 +0000

    sidebar added; posts are now varied

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,32 +1,33 @@
 import "./post.css"
 import {MoreVert} from "@mui/icons-material"
 
-export default function Post() {
+export default function Post({post}) {
+  console.log(post);
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
             <div className="postTopLeft">
-                <img src="/assets/Profile pics/ada prof pic.jpg" alt="" className="postCreatorImage" />
+                <img src={"/assets/Profile pics/ada prof pic.jpg"} alt="" className="postCreatorImage" />
                 <span className="postUserName">Ada Wong</span>
-                <span className="postDate">5 minutes ago</span>
+                <span className="postDate">{post.date}</span>
             </div>
             <div className="postTopRight">
                 <MoreVert/>
             </div>
         </div>
         <div className="postCentre">
-            <span className="postText">Good times Leon ;)</span>
-            <img src="/assets/posts/ada post.jpg" alt="" className="postImage" />
+            <span className="postText">{post?.desc}</span>
+            <img src={post.photo} alt="" className="postImage" />
         </div>
         <div className="postBottom">
             <div className="postBottomLeft">
                 <img src="/assets/like logo.png" alt="" className="reactionButton" />
                 <img src="/assets/heart logo.png" alt="" className="reactionButton" />
-                <span className="likeCounter">11.5k people like it</span>
+                <span className="likeCounter">{post.like} people like it</span>
             </div>
             <div className="postBottomRight">
-                <span className="commentsCounter">15 comments</span>
+                <span className="commentsCounter">{post.comment} comments</span>
             </div>
         </div>
       </div>
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,55 @@
-import "./rightbar.css"
+import "./rightbar.css";
 
 export default function Rightbar() {
   return (
     <div className="rightbar">
-      rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            {" "}
+            <b>Leon S. Kennedy</b> has a birthday today
+          </span>
+        </div>
+        <h4 className="onlineFriends">Friends online</h4>
+        <ul className="rightbarFriendslist">
+          <li className="rightbarFriend">
+            <div className="friendImgContainer">
+              <img src="/assets/Profile pics/ashley prof pic.png" alt="" className="friendProfImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="rightbarFriendUsername">Ashley Graham</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="friendImgContainer">
+              <img src="/assets/Profile pics/Leon prof pic.jpg" alt="" className="friendProfImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="rightbarFriendUsername">Leon S. Kennedy</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="friendImgContainer">
+              <img src="/assets/Profile pics/ashley prof pic.png" alt="" className="friendProfImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="rightbarFriendUsername">Ashley Graham</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="friendImgContainer">
+              <img src="/assets/Profile pics/ashley prof pic.png" alt="" className="friendProfImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="rightbarFriendUsername">Ashley Graham</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="friendImgContainer">
+              <img src="/assets/Profile pics/ashley prof pic.png" alt="" className="friendProfImg" />
+              <span className="onlineStatus"></span>
+            </div>
+            <span className="rightbarFriendUsername">Ashley Graham</span>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
